a3dd7b5cadc58a6236cc1e6470794d34ea3f7622
CaseyKelly <casey.kelly@colorado.edu>
2015-03-06 09:39:04 -0700

    All tests pass

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,10 +8,11 @@ class TasksController < ApplicationController
   def create
     task_params = params.require(:task).permit(:description, :due_date).merge(task_list_id: params[:task_list_id])
     @task = Task.new(task_params)
+    @task.task_list_id = params[:task_list_id]
     if @task.save
       redirect_to root_path, notice: "Task was created successfully!"
     else
-      render :new
+      render :new, notice: "No"
     end
   end
 
